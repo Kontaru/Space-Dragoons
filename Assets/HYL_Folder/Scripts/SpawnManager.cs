d07dad25b6be6b558195f53d9bf51cc6aaccc595
Unknown <drwafflesxd@gmail.com>
2018-02-07 14:05:00 +0000

    Spawn Manager

---
+++ Assets/HYL_Folder/Scripts/SpawnManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+
+    public GameObject[] spawnList;
+    public List<SpawnObject> spawns = new List<SpawnObject>(0);
+    SpawnObject current;
+    SpawnObject newspawn;
+
+    float cooldown = 0;
+    bool allLimit = true;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Update()
+    {
+
+        if (Time.time > cooldown + 5.0f)
+        {
+
+            ChoosePosition();
+
+            var enemy = (GameObject)Instantiate(current.enemyPrefab, current.pos, current.rot);
+
+            for (int i = 0; i < spawnList.Length; i++)
+            {
+                if (spawnList[i] == null)
+                    spawnList[i] = enemy;
+            }
+
+            cooldown = Time.time;
+        }
+    }
+
+    void ChoosePosition()
+    {
+        newspawn = spawns[Random.Range(0, spawns.Count)];
+        current = newspawn;
+    }
+}
---
+++ Assets/HYL_Folder/Scripts/SpawnObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public Vector3 pos;
+    public Quaternion rot;
+
+	// Use this for initialization
+	void Start () {
+
+        SpawnManager.instance.spawns.Add(this);
+        pos = transform.position;
+        rot = transform.rotation;
+	}
+
+}
