21e3b76c1e1d075db58e6ddb7456137483936c40
Oliver Knight <oliknight95@gmail.com>
2018-02-16 14:45:49 +0000

    OK: Menu Added

---
+++ Assets/OK_Folder/Scripts/OK_AdManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class OK_AdManager : MonoBehaviour
+{
+
+
+    public void ShowAd(bool bl_Play)
+    {
+        if (bl_Play == true)
+        {
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show("video", new ShowOptions() { resultCallback = HandleAdResult });
+            }
+        }
+    }
+
+    private void HandleAdResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Player Watched Ad");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Player Skipped ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Player Failed to launch ad");
+                break;
+        }
+    }
+}
---
+++ Assets/OK_Folder/Scripts/OK_MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OK_MenuManager : MonoBehaviour {
+
+    public void SceneLoad(string st_SceneName)
+    {
+        SceneManager.LoadScene(st_SceneName);
+    }
+    public void ExitAp()
+    {
+        Application.Quit();
+    }
+}
